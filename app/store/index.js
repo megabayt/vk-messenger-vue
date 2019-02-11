e3ad481b3a00b06e46933fc3b328e3faf9f494c9
Artemiy Martyanov <artem@bokus.ru>
2019-02-11 23:14:17 +0300

    add persistence

--- app/store/index.js
+++ app/store/index.js
@@ -1,9 +1,23 @@
 import Vue from 'nativescript-vue';
 import Vuex from 'vuex';
+import localStorage from 'nativescript-localstorage';
 import * as actions from './actions';
 import * as mutations from './mutations';
 
 Vue.use(Vuex);
+
+const NSVuexPersistent = store => {
+  // Init hook.
+  let storageStr = localStorage.getItem('ns-vuex-persistent');
+  if (storageStr) {
+   store.replaceState(JSON.parse(storageStr))
+  }
+  store.subscribe((mutation, state) => {
+   // Suscribe hook.
+   localStorage.setItem('ns-vuex-persistent', JSON.stringify(state));
+  })
+ };
+
 
 const store = new Vuex.Store({
   actions,
@@ -25,6 +39,7 @@ const store = new Vuex.Store({
       data: [],
     },
   },
+  plugins: [NSVuexPersistent],
 });
 
 export default store;
